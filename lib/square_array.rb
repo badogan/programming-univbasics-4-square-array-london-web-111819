7e371b991b96d6013daaa345117b319dcece67a9
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-04 13:07:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-   counter = 0 
-   while counter < array.length do
-     new_array[counter] = array[counter] ** 2 
-     
+  counter = 0 
+  while counter < array.length do
+     array[counter] = array[counter] ** 2 
+     counter = counter +1 
+  end
+  return array
 end
